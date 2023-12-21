3ae38c64594d51ab82a7f9e65ce6a8e7d44f3ab3
JacobZha <zhayang@xizhi-ai.com>
2023-12-21 23:01:19 +0800

    update: tailwindconfig; viteconfig; eslintconfig

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { Theme } from '@radix-ui/themes'
 import App from './App.tsx'
 import './index.css'
+import '@radix-ui/themes/styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Theme
+      style={{
+        width: '100%',
+        height: '100%',
+      }}
+      appearance="dark"
+      accentColor="green"
+      grayColor="sand"
+      radius="full"
+    >
+      <App />
+      {/* <ThemePanel /> */}
+    </Theme>
   </React.StrictMode>,
 )
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
+import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: [{ find: '@', replacement: fileURLToPath(new URL('./src', import.meta.url)) }],
+  },
+  server: {
+    port: 8080,
+  },
 })
